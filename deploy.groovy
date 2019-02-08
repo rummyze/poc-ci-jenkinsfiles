08f651411c2cf3560b281fa1aa23a47fbd6fd650
rummyze <Ruslan.Melnic@endava.com>
2019-02-08 16:38:53 +0200

    editing

--- deploy.groovy
+++ deploy.groovy
@@ -2,17 +2,20 @@
 import com.library.Shared
 
 def shared = new Shared(this)
-def versions =["ft1","ft2"]
+def versions = ["2.2.5","2.1.4","1.0.0"]
 def envs = ["ft1","ft2"]
 def repoName = "poc-ci-jenkinsfiles"
 def repoOwner = "rummyze"
-def m = APP_VERSIONS =~ /(\d).(\d).(\d)/
+ 
+def s = APP_VERSIONS
+def m = s =~ /(\d).(\d).(\d)/
 def APP_VERSION = m[0][0]
 
 pipeline {
     agent any
 
     parameters { 
+        choice(name: 'APP_VERSION', choices: versions, description: 'app version')
         choice(name: 'ENV', choices: envs, description: 'app version')
     }
 
@@ -33,7 +36,6 @@ pipeline {
                 stage('download service-1') {
                     steps {
                         script {
-                        
                             shared.downloadHelloService(params.APP_VERSION)
                         }
                     }
