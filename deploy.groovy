2e414020c25e88a5b4adb4405fe06622118b7ab8
RUSLAN MELNIC <rummyze@gmail.com>
2019-02-18 14:19:36 +0200

    Update deploy.groovy

--- deploy.groovy
+++ deploy.groovy
@@ -5,7 +5,7 @@ def shared = new Shared(this)
 def versions = ["2.2.8","2.1.4","1.0.2"]
 def envs = ["ft1","ft2"]
 def repoName = "poc-ci-jenkinsfiles"
-def repoOwner = "edbighead"
+def repoOwner = "rummyze"
  
 pipeline {
     agent any
